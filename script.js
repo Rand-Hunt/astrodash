f0646ca13faa4545b6fa2d8343d1e105ddd8e1b6
Kasey Chang <kschang77@yahoo.com>
2020-03-31 15:20:39 -0700

    20200331 KC Sentiment analysis throwing errors

--- script.js
+++ script.js
@@ -1,31 +1,63 @@
 // Once document is loaded
-$(document).ready(function () {
+    var resp = "";
 
+$(document).ready(function () {
     var sign = "gemini";
 
     $(".submit").on("click", function (event) {
 
         event.preventDefault();
 
-        // Astro sign input
-        // sign = $("#sign").val();
-        // sign = "gemini";
-
     });
 
+    function getData2() {
+      console.log("resp2 = " + resp);
+      token = "8921d8d3e0274f0997aa91de967aca75";
 
-    function getData() {
+      queryURL2 =
+        "https://api.dandelion.eu/datatxt/nex/v1/?text=" +
+        encodeURI(resp) +
+        "&min_confidence=0.5";
+        "&top_entities=4&include=abstract%2Ctypes%2Ccategories"+
+        "&token=" +token;
 
-        var queryURL = "https://ohmanda.com/api/horoscope/" + sign + "/";
+      console.log(queryURL2);
+          $.ajax({
+              type: "POST",
+              url: queryURL2,
+              dataType: "json"
+          }).then(function (response) {
+              console.log(response);
+              // console.log(reponse.description);
+          });
+    }
+ 
+    function getData(options,callback) {
 
+        // var queryURL = "https://ohmanda.com/api/horoscope/" + sign + "/";
+
+        // var queryURL = "https://cors-anywhere.herokuapp.com/https://sandipbgt.com/theastrologer/api/horoscope/"+sign+"/today/"
+// var res;
+
+ var queryURL = "https://aztro.sameerkumar.website?sign=" + sign + "&day=today";
+
+        
         $.ajax({
+            type: "POST",
             url: queryURL,
-            method: "GET"
+            dataType: "json"
         }).then(function (response) {
-            console.log(response);
-
+            options = response.description;
+            resp = response.description;
+            // console.log(response);
+            // console.log(response.description);
+            console.log("options="+options)
+            console.log("resp="+resp)
+            callback();
         });
     }
 
-    getData();
-});
+    getData(resp,getData2);
+    // getData2();
+
+    });
